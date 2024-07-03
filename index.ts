50f324dc4c5f45afd9fa253dcb4f519e8a8b15f8
yecksin <yecksin@gmail.com>
2024-07-03 08:05:04 -0500

    Express sockets

---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import express from 'express';
+import cors from 'cors';
+
+const server = new Server();
+const {app} = server;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cors({origin: true, credentials: true}))
+
+server.start(() => {
+  console.log(`Servidor corriendo en http://localhost:${server.port}`);
+});
+
+app.get('/example', (req, res) => {
+    res.send('Hola Mundo');
+  });
+
+app.use('/', router);
+  
